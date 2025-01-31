3b732fe6427550af1f8f5ce1c3e6399692b5c611
Aswanth Allu <AswanthAllu@users.noreply.github.com>
2025-01-31 10:00:09 +0530

    Updated new Resume

--- src/components/Resume/index.js
+++ src/components/Resume/index.js
@@ -3,7 +3,7 @@ import { FaDownload } from "react-icons/fa6";
 const Resume =()=>{
     return (
 
-            <a href="https://drive.google.com/file/d/1-YZ0OBFJ5pWZTvHAte2ICFdQTNd55yat/view?usp=drivesdk"  >
+            <a href="https://drive.google.com/file/d/127dgiw-qfYBeG-XFwiPidOC0sgkdLdOy/view?usp=drivesdk"  >
                 <div className="resume-button-con fade-in-to-right ">
                     <div className='resume-button'>Resume</div>
                     <FaDownload size={25} />
